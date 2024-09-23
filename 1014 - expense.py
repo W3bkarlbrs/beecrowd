29fef0226f6749c39607aa2260e2f36962abaae5
Karoline <karolbarros0910@gmail.com>
2024-09-23 20:23:45 -0300

    day 3

---
+++ 1014 - expense.py
@@ -0,0 +1,12 @@
+#distancia
+x = int(input())
+
+#valor
+y = float(input())
+
+#consumo medio
+avg_expense = round(x/y, 3)
+
+#mensagem final
+print(avg_expense, "km/l")
+
---
+++ 2413 - internet_search.py
@@ -0,0 +1,8 @@
+#numero de pessoas que clicaram no 3 link
+t = int(input())
+
+#multiplicando para chegar ao n de pessoas que clicaram no 1
+saida = t*4
+
+#apresentando resultado
+print(saida)
